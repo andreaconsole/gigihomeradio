e6f890374c85c1b4f81a13de64ff1f48dcc07c6e
andreaconsole <andreaconsole@gmail.com>
2025-06-08 11:32:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,13 @@
 echo "Copying scripts to /home/osmc..."
 cp radio_player.py gpio_radio_button.py /home/osmc/
 
-echo "Installing Kodi autoexec..."
-mkdir -p /home/osmc/.kodi/userdata/
-cp autoexec.py /home/osmc/.kodi/userdata/
-
 chmod +x /home/osmc/*.py
 
-echo "Installing systemd service..."
-sudo cp radio-buttons.service /etc/systemd/system/
-sudo systemctl daemon-reexec
+# Install and enable systemd services
+echo "Installing systemd services..."
+sudo cp gpio_radio_button.service gpio_rotary_volume.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl enable radio-buttons.service
-sudo systemctl start radio-buttons.service
-sudo systemctl enable rotary-volume.service
-sudo systemctl start rotary-volume.service
+sudo systemctl enable gpio_radio_button.service gpio_rotary_volume.service
+sudo systemctl start gpio_radio_button.service gpio_rotary_volume.service
 
 echo "Done. To edit your stations, modify /media/usb/stations.txt"
